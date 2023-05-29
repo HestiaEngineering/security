d6e15d767185ccade7f403caafa2cc9ac80e44d3
danielalexis <danielalexis.o.pereira@gmail.com>
2023-05-29 21:50:26 +0100

    add db diagram

--- routes/index.ts
+++ routes/index.ts
@@ -3,6 +3,9 @@ import { Context } from "https://deno.land/x/oak@v12.4.0/context.ts";
 export class index {
   public static get(ctx: Context) {
     ctx.response.type = "json";
-    ctx.response.body = { "hello": "world" };
+    ctx.response.body = {
+      "module": "security",
+      "version": "alpha",
+    };
   }
 }
