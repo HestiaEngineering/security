3fe7cac39ea17930adb20eade32cbff88d017f5f
danielalexis <danielalexis.o.pereira@gmail.com>
2023-05-30 20:48:01 +0100

    db sql

---
+++ db/internal_api.sql
@@ -0,0 +1,53 @@
+/*
+ *  DBMS: `PostgreSQL 15`
+ *  Database: `security`
+ *  Tables: `internal_api`, `internal_api_key`, `internal_api_key_role`, `internal_api_role`
+ */
+
+CREATE DATABASE `security` IF NOT EXISTS;
+CREATE SCHEMA `security` IF NOT EXISTS;
+
+CREATE TABLE `internal_api` (
+    `id` uuid NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `status` varchar(255) NOT NULL,
+    `created_at` timestamp NOT NULL,
+    `updated_at` timestamp NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `internal_api_key` (
+    `id` uuid NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `key` varchar(255) NOT NULL,
+    `status` varchar(255) NOT NULL,
+    `created_at` timestamp NOT NULL,
+    `updated_at` timestamp NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `internal_api_key_role` (
+    `internal_api_key_id` uuid NOT NULL,
+    `internal_api_role_id` uuid NOT NULL,
+    PRIMARY KEY(`internal_api_key_id`, `internal_api_role_id`),
+    CONSTRAINT `fk_internal_api_key_id` FOREIGN KEY (`internal_api_key_id`) REFERENCES `internal_api_key`(`id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_internal_api_role_id` FOREIGN KEY (`internal_api_role_id`) REFERENCES `internal_api_role`(`id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `internal_api_role` (
+    `id` uuid NOT NULL,
+    `name` varchar(255) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `status` varchar(255) NOT NULL,
+    `created_at` timestamp NOT NULL,
+    `updated_at` timestamp NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE INDEX `idx_internal_api_key_id` ON `internal_api_key_role`(`internal_api_key_id`);
+CREATE INDEX `idx_internal_api_role_id` ON `internal_api_key_role`(`internal_api_role_id`);
+CREATE INDEX `idx_internal_api_key_id` ON `internal_api_key`(`id`);
+CREATE INDEX `idx_internal_api_role_id` ON `internal_api_role`(`id`);
+CREATE INDEX `idx_internal_api_id` ON `internal_api_key`(`id`);
--- routes/internal-api/index.ts
+++ routes/internal-api/index.ts
@@ -1,4 +1,7 @@
+import { Context } from "../../deps.ts";
+
 export class InternalApiIndex {
-  public static get() {
+  public static get(ctx: Context) {
+    ctx.response.body = {};
   }
 }
