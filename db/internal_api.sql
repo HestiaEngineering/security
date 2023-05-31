a6e5ad9a54d2866c0b154ecf201bbe06b691dfff
danielalexis <danielalexis.o.pereira@gmail.com>
2023-05-31 21:12:17 +0100

    update database

--- db/internal_api.sql
+++ db/internal_api.sql
@@ -1,53 +1,55 @@
 /*
- *  DBMS: `PostgreSQL 15`
- *  Database: `security`
- *  Tables: `internal_api`, `internal_api_key`, `internal_api_key_role`, `internal_api_role`
+ *  DBMS: "PostgreSQL 15"
+ *  Database: "security"
+ *  Tables: "internal_api", "internal_api_key", "internal_api_key_role", "internal_api_role"
  */
 
-CREATE DATABASE `security` IF NOT EXISTS;
-CREATE SCHEMA `internal_api` IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS "security";
+CREATE SCHEMA IF NOT EXISTS "internal_api";
 
-CREATE TABLE `internal_api`.`api` (
-    `id` uuid NOT NULL DEFAULT uuid_generate_v4(),
-    `name` varchar(255) NOT NULL,
-    `description` varchar(255) NOT NULL,
-    `status` varchar(255) NOT NULL,
-    `created_at` timestamp NOT NULL,
-    `updated_at` timestamp NOT NULL,
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS "internal_api"."api" (
+    "id" uuid NOT NULL,
+    "name" varchar(255) NOT NULL,
+    "description" varchar(255) NOT NULL,
+    "status" varchar(255) NOT NULL,
+    "created_at" timestamp NOT NULL,
+    "updated_at" timestamp NOT NULL,
+    PRIMARY KEY("id")
 );
 
-CREATE TABLE `internal_api_key` (
-    `id` uuid NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `description` varchar(255) NOT NULL,
-    `key` varchar(255) NOT NULL,
-    `status` varchar(255) NOT NULL,
-    `created_at` timestamp NOT NULL,
-    `updated_at` timestamp NOT NULL,
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS "internal_api"."key" (
+    "id" uuid NOT NULL,
+    "name" varchar(255) NOT NULL,
+    "description" varchar(255) NOT NULL,
+    "key" varchar(255) NOT NULL,
+    "status" varchar(255) NOT NULL,
+    "created_at" timestamp NOT NULL,
+    "updated_at" timestamp NOT NULL,
+    PRIMARY KEY("id")
 );
 
-CREATE TABLE `internal_api_key_role` (
-    `internal_api_key_id` uuid NOT NULL,
-    `internal_api_role_id` uuid NOT NULL,
-    PRIMARY KEY(`internal_api_key_id`, `internal_api_role_id`),
-    CONSTRAINT `fk_internal_api_key_id` FOREIGN KEY (`internal_api_key_id`) REFERENCES `internal_api_key`(`id`) ON DELETE CASCADE,
-    CONSTRAINT `fk_internal_api_role_id` FOREIGN KEY (`internal_api_role_id`) REFERENCES `internal_api_role`(`id`) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS "internal_api"."role" (
+    "id" uuid NOT NULL,
+    "name" varchar(255) NOT NULL,
+    "description" varchar(255) NOT NULL,
+    "status" varchar(255) NOT NULL,
+    "created_at" timestamp NOT NULL,
+    "updated_at" timestamp NOT NULL,
+    PRIMARY KEY("id")
 );
 
-CREATE TABLE `internal_api_role` (
-    `id` uuid NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `description` varchar(255) NOT NULL,
-    `status` varchar(255) NOT NULL,
-    `created_at` timestamp NOT NULL,
-    `updated_at` timestamp NOT NULL,
-    PRIMARY KEY(`id`)
+CREATE TABLE IF NOT EXISTS "internal_api"."key_role" (
+    "key" uuid NOT NULL,
+    "role" uuid NOT NULL,
+    PRIMARY KEY("key", "role"),
+    CONSTRAINT "fk_internal_api_key_id" FOREIGN KEY ("internal_api_key_id") REFERENCES "internal_api_key"("id") ON DELETE CASCADE,
+    CONSTRAINT "fk_internal_api_role_id" FOREIGN KEY ("internal_api_role_id") REFERENCES "internal_api_role"("id") ON DELETE CASCADE
 );
 
-CREATE INDEX `idx_internal_api_key_id` ON `internal_api_key_role`(`internal_api_key_id`);
-CREATE INDEX `idx_internal_api_role_id` ON `internal_api_key_role`(`internal_api_role_id`);
-CREATE INDEX `idx_internal_api_key_id` ON `internal_api_key`(`id`);
-CREATE INDEX `idx_internal_api_role_id` ON `internal_api_role`(`id`);
-CREATE INDEX `idx_internal_api_id` ON `internal_api_key`(`id`);
+
+
+CREATE INDEX "idx_internal_api_key_id" ON  "internal_api"."key_role"("internal_api_key_id");
+CREATE INDEX "idx_internal_api_role_id" ON "internal_api"."role"("id");
+CREATE INDEX "idx_internal_api_id" ON      "internal_api"."key"("id");
+CREATE INDEX "idx_internal_api_role_id" ON "internal_api"."key_role"("internal_api_role_id");
+CREATE INDEX "idx_internal_api_key_id" ON   "internal_api"."key"("id");
