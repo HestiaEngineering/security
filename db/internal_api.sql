93cdaf58f81d0c9ef7aaf02006c1cdc7c1702fde
danielalexis <danielalexis.o.pereira@gmail.com>
2023-05-31 22:55:30 +0100

    DB schema changes

--- db/internal_api.sql
+++ db/internal_api.sql
@@ -9,23 +9,20 @@ CREATE SCHEMA IF NOT EXISTS "internal_api";
 
 CREATE TABLE IF NOT EXISTS "internal_api"."api" (
     "id" uuid NOT NULL,
+    "code" varchar(255) NOT NULL,
     "name" varchar(255) NOT NULL,
-    "description" varchar(255) NOT NULL,
-    "status" varchar(255) NOT NULL,
-    "created_at" timestamp NOT NULL,
-    "updated_at" timestamp NOT NULL,
+    "description" TEXT NOT NULL,
     PRIMARY KEY("id")
 );
 
 CREATE TABLE IF NOT EXISTS "internal_api"."key" (
     "id" uuid NOT NULL,
-    "name" varchar(255) NOT NULL,
-    "description" varchar(255) NOT NULL,
+    "api" uuid NOT NULL,
     "key" varchar(255) NOT NULL,
-    "status" varchar(255) NOT NULL,
-    "created_at" timestamp NOT NULL,
-    "updated_at" timestamp NOT NULL,
+    "name" varchar(255) NOT NULL,
+    "description" TEXT NOT NULL,
     PRIMARY KEY("id")
+    CONSTRAINT "fk_internal_api_api_id" FOREIGN KEY ("api") REFERENCES "internal_api"."api"("id") ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "internal_api"."role" (
@@ -46,8 +43,7 @@ CREATE TABLE IF NOT EXISTS "internal_api"."key_role" (
     CONSTRAINT "fk_internal_api_role_id" FOREIGN KEY ("internal_api_role_id") REFERENCES "internal_api_role"("id") ON DELETE CASCADE
 );
 
-
-
+-- Is this really needed?
 CREATE INDEX "idx_internal_api_key_id" ON  "internal_api"."key_role"("internal_api_key_id");
 CREATE INDEX "idx_internal_api_role_id" ON "internal_api"."role"("id");
 CREATE INDEX "idx_internal_api_id" ON      "internal_api"."key"("id");
